ea7186604cb317dff99beb755260150c08fa0dda
Maksim124512M <max1996az@gmail.com>
2024-08-10 13:48:27 +0300

    Signup and profile

--- hospital/users/urls.py
+++ hospital/users/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from users import views
+from users.views import ProfileView, SignupView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('profile/', ProfileView.as_view(), name='profile'),
 ]
